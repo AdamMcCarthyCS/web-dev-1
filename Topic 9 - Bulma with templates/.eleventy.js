0f00b242472c673bc09dbd04687cb01191ad43ae
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-12-13 13:25:13 +0000

    fix(config): Add import of eleventy navigation plugin

--- Topic 9 - Bulma with templates/.eleventy.js
+++ Topic 9 - Bulma with templates/.eleventy.js
@@ -1,3 +1,5 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
